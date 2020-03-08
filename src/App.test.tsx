33ffadf1a94a18bf4d418fd20d5a01b339ccfeee
Jeff Skelton <jeff@jeffskelton.net>
2020-03-07 20:07:57 -0800

    get tsyringe working with CRA

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import {ExampleService} from "./example-service";
+import {Foo} from "./foo-service";
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('testing my service', () => {
+    const ex = new ExampleService({
+        doTheThing: () => {
+            return 'it works'
+        }
+    } as Foo);
+    expect(ex.doAFooThing()).toEqual('it works')
 });
